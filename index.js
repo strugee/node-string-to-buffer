af77e47c0c3a90febbb52a004497fa896e5e4f83
Alex Jordan <alex@strugee.net>
2017-04-24 14:06:54 -0400

    Write the module

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+module.exports = function stringToBuffer(str) {
+	// Node 6+ uses Buffer.from but earlier versions don't have this, so we fallback
+	file.contents = Buffer.from ? Buffer.from(str) : new Buffer(str);
+}
