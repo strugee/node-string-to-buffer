6c02764ba9551765ad10a1904effffa58ed992ad
AJ Jordan <alex@strugee.net>
2017-07-11 17:44:38 -0700

    Enable strict mode

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 module.exports = function stringToBuffer(str) {
 	// Node 6+ uses Buffer.from but earlier versions don't have this, so we fallback
 	return Buffer.from ? Buffer.from(str) : new Buffer(str);
